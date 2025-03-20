2e2c36c7f0a43b78a6d10990828c744f81eb4244
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-03-20 17:55:35 +0100

    Revert "Remove more permissions configs"
    The phone doesn't go past the boot animation and just reboots. Most likely one of these configs is the cause.

--- scripts/modify_configs/system_ext.sh
+++ scripts/modify_configs/system_ext.sh
@@ -20,30 +20,6 @@ fi
 REMOVE_CONFIGS=(
 
     "${ETC_PATH}/init/init.spotifyprops.rc"
-
-    "${ETC_PATH}/default-permissions/default-permission-com.motorola.ccc.notification.xml"
-    "${ETC_PATH}/sysconfig/com.motorola.paks.powersave.xml"
-    "${ETC_PATH}/sysconfig/com.motorola.ccc.notification.xml"
-    "${ETC_PATH}/sysconfig/com.motorola.ccc.devicemanagement.xml"
-    "${ETC_PATH}/sysconfig/hiddenapi-allowlist-com.motorola.paks.xml"
-    # "${ETC_PATH}/sysconfig/hiddenapi-whitelist-com.motorola.ccc.mainplm.xml" # Moto services. Have to check eventual dependencies with Launcher/Gestures and other moto stuff that we keep
-    "${ETC_PATH}/sysconfig/hiddenapi-whitelist-com.motorola.demo.xml"
-    "${ETC_PATH}/sysconfig/hiddenapi-whitelist-com.motorola.livewallpaper.xml"
-    "${ETC_PATH}/sysconfig/whitelist_com.motorola.motocare.xml"
-    "${ETC_PATH}/sysconfig/main-afw.config.xml" # Moto services too
-
-
-    "${ETC_PATH}/permissions/afw-com.dti.motorola.xml"
-    "${ETC_PATH}/permissions/afw-com.inmobi.installer.xml"
-    "${ETC_PATH}/permissions/afw-com.motorola.livewallpaper.xml"
-    "${ETC_PATH}/permissions/feature-com.motorola.motolivewallpaper.xml"
-    "${ETC_PATH}/permissions/privapp-permissions-com.motorola.bach.modemstats.xml"
-    "${ETC_PATH}/permissions/privapp-permissions-com.motorola.ccc.devicemanagement.xml"
-    "${ETC_PATH}/permissions/privapp-permissions-com.motorola.ccc.notification.xml"
-    "${ETC_PATH}/permissions/privapp-permissions-com.motorola.demo.xml"
-    "${ETC_PATH}/permissions/privapp-permissions-com.motorola.livewallpaper3.xml"
-    "${ETC_PATH}/permissions/privapp-permissions-com.motorola.motocare.xml"
-    "${ETC_PATH}/permissions/privapp-permissions-com.motorola.paks.xml"
 )
 
 for item in "${REMOVE_CONFIGS[@]}"; do
